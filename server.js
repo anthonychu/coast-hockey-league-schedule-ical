497720792592402f929be02cab0817593b5b166c
Anthony Chu <anthony@anthonychu.ca>
2015-01-12 22:10:34 -0800

    Add editorconfig file, reformat, refactor

--- server.js
+++ server.js
@@ -9,46 +9,55 @@ request(url, processScheduleHtml);
 
 
 function processScheduleHtml(err, resp, html) {
-  var $ = cheerio.load(html);
-  var games = [];
+    var $ = cheerio.load(html);
+    var games = [];
   
-  var title = $('#lblTitle').text();
-  var teamName = /schedule:(.+)/i.exec(title)[1].trim();
+    var title = $('#lblTitle').text();
+    var teamName = /schedule:(.+)/i.exec(title)[1].trim();
   
-  var gameRows = $('#pnlViewTeamSchedules table:first-child tr');
+    var gameRows = $('#pnlViewTeamSchedules table:first-child tr');
   
-  gameRows.map(function(idx, row) {
-    var gameRow = $(row);
+    gameRows.map(function(idx, row) {
+        var gameRow = $(row);
     
-    if (!gameRow.has('td.table-content1, td.table-content2').length) return;
+        if (!gameRow.has('td.table-content1, td.table-content2').length) return;
     
-    var cells = gameRow.find('td');
+        var cells = gameRow.find('td');
+
+        var date = moment(cells.first().text());
+        var rink = cells.eq(1).text().trim();
+        var homeTeamName = cells.eq(2).find('a').text().trim();
+        var awayTeamName = cells.eq(3).find('a').text().trim();
+        var score = parseScoreText(cells.eq(4).text().trim());
+        var result = getResult(score.home, score.away, teamName === homeTeamName)
+
+        console.log(date.toString(), rink, homeTeamName, awayTeamName, score.home, score.away, result);
+    });
+}
+
+function parseScoreText(scoreText) {
+    var homeScore, awayScore;
     
-    var date = moment(cells.first().text());
-    var rink = cells.eq(1).text().trim();
-    var home = cells.eq(2).find('a').text().trim();
-    var away = cells.eq(3).find('a').text().trim();
-    var score = cells.eq(4).text().trim();
-    
-    var match = /(\d+)\s+-\s+(\d+)/.exec(score);
-    var result = '';
+    var match = /(\d+)\s+-\s+(\d+)/.exec(scoreText);
     if (match) {
-      var homeScore = parseInt(match[1]);
-      var awayScore = parseInt(match[2]);
-      
-      if (homeScore === awayScore) {
-        result = 'Tie';
-      } else if (teamName === home && homeScore > awayScore ||
-                 teamName === away && awayScore > homeScore) {
-        result = 'Win';
-      } else {
-        result = 'Loss';
-      }
-      
-      home += " " + homeScore;
-      away += " " + awayScore;
+        homeScore = parseInt(match[1]);
+        awayScore = parseInt(match[2]);
     }
     
-    console.log(date.toString(), rink, home, away, result);
-  });
+    return {
+        home: homeScore,
+        away: awayScore
+    };
+}
+
+function getResult(homeScore, awayScore, isHomeTeam) {
+    if (homeScore === awayScore) {
+        result = 'Tie';
+    } else if (isHomeTeam && homeScore > awayScore ||
+              !isHomeTeam && awayScore > homeScore) {
+        result = 'Win';
+    } else {
+        result = 'Loss';
+    }
+    return result;
 }
